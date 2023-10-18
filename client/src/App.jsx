7ed57d3cd95ba59fe18cc03aafe5b1682d268a17
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-10-18 11:35:45 +1100

    refactored globalstate.js, added store.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,11 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+// import { setContext } from '@apollo/client/link/context';
 
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+import StoreProvider from './utils/GlobalState';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
--- client/src/utils/GlobalState.jsx
+++ client/src/utils/GlobalState.jsx
@@ -1,23 +1,30 @@
-import { createContext, useContext, useReducer } from "react";
-import { reducer } from './reducers'
+// import { createContext, useContext, useReducer } from "react";
+// import { reducer } from './reducers'
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+import { Provider } from 'react-redux';
+import store from './store';
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useReducer(reducer, {
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useReducer(reducer, {
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: '',
+//   });
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
 
-export { StoreProvider, useStoreContext };
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
+
+// export { StoreProvider, useStoreContext };
+
+export default function StoreProvider(props) {
+  return <Provider store={store} {...props} />;
+}
